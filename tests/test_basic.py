507dcf689c6970e0827071e07649fed26ec4f140
ranmaru22 <alex@MBP.local>
2020-01-15 20:33:10 +0100

    Prepared unit tests [wip]

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,10 +3,10 @@
 import unittest
 from ...main import main
 
+
 class TestLinkGathering(unittest.TestCase):
     """Tests if the corrent number of links is returned."""
     # Sample URI: /r/learnprogramming/comments/eojk97
-
 
     def test_full_cycle(self):
         pass
